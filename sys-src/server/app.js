abccc94d399cfb1e9582a82db2195703bbbf9693
Aaron Kreuzer <a.kreuzer@oth-aw.de>
2021-05-27 15:52:47 +0200

    #10 palette example

--- sys-src/server/app.js
+++ sys-src/server/app.js
@@ -10,12 +10,21 @@ mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
+db.on('connected', console.log.bind(console, 'Connected to mongoDB'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Router
+let palette = require('./routes/paletteRoute')
+
+//Middleware
+app.use(express.json());
 
 //App
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+app.use('/palette', palette)
 
 app.listen(3000, function() {
     console.log('listening on 3000')
