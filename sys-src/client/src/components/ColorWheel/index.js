e9236b76f39c93aa4a4638ec2769f89193285f83
Harun T <h.tacli@oth-aw.de>
2021-06-14 11:35:51 +0200

    add colorwheel

--- sys-src/client/src/components/ColorWheel/index.js
+++ sys-src/client/src/components/ColorWheel/index.js
@@ -1,3 +1,24 @@
 /**
  * Enthält das Farbrad.
  */
+
+ import iro from '@jaames/iro';
+ //import ColorPicker from 'irojs-react'
+
+function ColorWheel() {
+    var colorPicker = new iro.ColorPicker('#picker');
+
+    return (
+        <div id="picker"></div>
+    );
+  }
+
+  export default ColorWheel;
+
+
+  
+ /*class Example extends Component {
+  render() {
+    return <ColorPicker />
+  }
+}*/
