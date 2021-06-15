ae008b318acd2ffa57fc368a119a0d72b1a0ad32
Anna-Lena Gassner <a.gassner@oth-aw.de>
2021-06-15 16:54:35 +0200

    updated component button and fixed failure in ratingController
    Former-commit-id: 7de534f7709dddc16b2725ef30df240386fdd1f9

--- sys-src/client/src/components/Button/index.jsx
+++ sys-src/client/src/components/Button/index.jsx
@@ -8,18 +8,24 @@ import "./index.css";
 
 export function Button(props) {
     //get values for title and colors from props
-  const { title, colors } = props;
+  const { title, colors, name } = props;
 
   //function for submitting colors to backend; not created yet
-  const submitPalette = (savePalette) => {
-    alert(savePalette);
+  const submitPalette = (saveColor, saveName) => {
+    fetch('http://localhost:5000/palette/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify({name: saveName, color: saveColor})
+    }).then(console.log('Die zu speichernden Farben sind: ' + saveColor + '. Der zu speichernde Name ist: ' + saveName + '.'));
   };
 
   return (
     <button
       className="buttonStyle"
       type="button"
-      onClick={() => submitPalette(colors)}
+      onClick={() => submitPalette(colors, name)}
     >
       <Title>{title}</Title>
     </button>
