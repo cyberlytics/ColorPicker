a04e997650b00f764a9cf186a0bea0cd9abf0fdc
Gassner, Anna-Lena <a.gassner@oth-aw.de>
2021-06-15 16:54:35 +0200

    updated component button and fixed failure in ratingController

--- sys-src/server/controllers/ratingController.js
+++ sys-src/server/controllers/ratingController.js
@@ -14,29 +14,34 @@ exports.rating_add = function(req, res){
         if(err) return console.error(err);
         console.log('New Rating added');
         res.json(newRating);
-    });
-
-    //Groups all documents that have the same pallet ID and then calculates the average value of the field rating
-    ratingModell.aggregate([{
-        "$group": {
-            "_id": ObjectID(palette_id),
-            "avg_rating": {
-                "$avg": "$rating"
+        //aggregates all ratings where the palette_id, is the palette_id of the new rating then groups this docs and calculates the new average
+        ratingModell.aggregate([
+            {
+                $match: {
+                    palette_id: ObjectID(palette_id)
+                },
+            }, {
+                $group: {
+                    _id: "$palette_id", 
+                    avg_rating: {
+                        $avg: "$rating"
+                    }
+                }
             }
-        }
-    }]).then(result => {
-        //if there was a result it will be worked with further
-        if(result){
-            //Query for the palette collection to find the palette with the correct _id
-           let query = {_id: ObjectID(palette_id)};
-
-           //Update variable that assigns the new calculated value to the avg_rating field.
-           let update = {
-               $set: {
-                   avg_rating: result[0].avg_rating
-               }
-           };
-           //Calls the updateOne function of mongoose with the values from query and update
-           paletteModell.updateOne(query, update).exec();
-    }}).catch(err => console.error(`Failed to find Document: ${err}`));
+        ]).then(result => {
+            //if there was a result it will be worked with further
+            if(result){
+                //Query for the palette collection to find the palette with the correct _id
+               let query = {_id: `${ObjectID(palette_id)}`};
+    
+               //Update variable that assigns the new calculated value to the avg_rating field.
+               let update = {
+                   $set: {
+                       avg_rating: result[0].avg_rating
+                   }
+               };
+               //Calls the updateOne function of mongoose with the values from query and update
+               paletteModell.updateOne(query, update).exec();
+        }}).catch(err => console.error(`Failed to find Document: ${err}`));
+    });
 };
