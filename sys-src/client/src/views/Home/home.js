7016293442b9ed8def9f3a91be8d7fd6efefbf7f
Patrick Pohl <p.pohl@oth-aw.de>
2021-06-18 12:12:16 +0200

    Added Slider/SliderContainer
    Former-commit-id: 39c6b9f700964c2b6f91279e2042df083cea9d56

--- sys-src/client/src/views/Home/home.js
+++ sys-src/client/src/views/Home/home.js
@@ -1,10 +1,14 @@
 import "./home.css"
+import SliderContainer from "../../components/SliderContainer/index.jsx"
 
 function Home() {
     return (
       <div>
         <h2 className="home">Home</h2>
+        <SliderContainer></SliderContainer>
       </div>
     );
   }
   export default Home;
+
+  
