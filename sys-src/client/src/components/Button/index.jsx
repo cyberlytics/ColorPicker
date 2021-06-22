2369a30e94d0c5906ee0a200ddba9d9556d5ef2f
Anna-Lena Gassner <a.gassner@oth-aw.de>
2021-06-22 16:03:46 +0200

    updated component button and view createPalette can now save palettes with save dialog
    Former-commit-id: a81afb74e153ba32f96d564fcaedd94ff1385363

--- sys-src/client/src/components/Button/index.jsx
+++ sys-src/client/src/components/Button/index.jsx
@@ -8,24 +8,14 @@ import "./index.css";
 
 export function Button(props) {
     //get values for title and colors from props
-  const { title, colors, name } = props;
-
-  //function for submitting colors to backend; not created yet
-  const submitPalette = (saveColor, saveName) => {
-    fetch('http://localhost:5000/palette/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      },
-      body: JSON.stringify({name: saveName, color: saveColor})
-    }).then(console.log('Die zu speichernden Farben sind: ' + saveColor + '. Der zu speichernde Name ist: ' + saveName + '.'));
-  };
+  const { title, onClick } = props;
 
   return (
     <button
       className="buttonStyle"
       type="button"
-      onClick={() => submitPalette(colors, name)}
+      // onClick={() => submitPalette(colors, name)}
+      onClick={onClick}
     >
       <Title>{title}</Title>
     </button>
