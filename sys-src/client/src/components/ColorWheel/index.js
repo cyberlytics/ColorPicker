151ad0ad5eacd0eb36d65e512b8d256f75a9fd96
Harun Tacli <h.tacli@oth-aw.de>
2021-06-24 17:55:24 +0200

    Update index.js
    Former-commit-id: 509a75bffa767acc55c97e0315d20990f07a9424

--- sys-src/client/src/components/ColorWheel/index.js
+++ sys-src/client/src/components/ColorWheel/index.js
@@ -1,42 +1,37 @@
 /**
  * Enthält das Farbrad.
  */
-import React from "react";
-import iro from "@jaames/iro";
-import "./colorwheel.css";
+ import React from "react";
+ import iro from "@jaames/iro";
+ import './colorwheel.css'
 
-class ColorWheel extends React.Component {
-  constructor(props) {
+ class ColorWheel extends React.Component {
+
+  constructor(props){
     super(props);
     this.state = {
-      colors: ["#f00"],
-      newColor: "",
-    };
+      colors: ["#f00","#ffff","ff22", "ff32"]
+    }
   }
 
-  componentDidMount() {
-    var colorPicker = new iro.ColorPicker(this.el, {
-      // Set the size of the color picker
-      width: 320,
-      // Set the initial color to pure red
-      colors: this.state.colors,
-    });
+   componentDidMount() {
+  var colorPicker = new iro.ColorPicker(this.el, {
 
-    colorPicker.on("color:change", function (color) {
-      // log the current color as a HEX string
-      console.log(color.hexString);
-      this.setState({ newColor: color.hexString });
-      this.onColorChange();
-      console.log(this.state.newColor);
-    });
-  };
+    // Set the size of the color picker
+    width: 320,
+    // Set the initial color to pure red
+    colors: this.state.colors
+  });
 
-  onColorChange=()=> {
-    this.props.callbackFromParent(this.state.newColor);
-  }
-
-  render() {
-    return <div ref={(el) => (this.el = el)} />;
-  }
-}
-export default ColorWheel;
+  colorPicker.on('color:change', function(color) {
+    // log the current color as a HEX string
+    console.log(color.hexString);
+  });
+   }
+ 
+   render() {
+     console.log(this.props.onColorChange);
+     return <div ref={el => (this.el = el)} />;
+   }
+ }
+ export default ColorWheel;
