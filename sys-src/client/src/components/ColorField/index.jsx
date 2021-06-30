8beaf40bff7bafe28c3bd594f2381c02ab0b3eca
Fabi <f.meiler1@oth-aw.de>
2021-06-30 20:02:02 +0200

    copy button added

--- sys-src/client/src/components/ColorField/index.jsx
+++ sys-src/client/src/components/ColorField/index.jsx
@@ -7,19 +7,21 @@
 import "./index.css";
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 class ColorField extends Component {
   render() {
-    const iconTrash = <FontAwesomeIcon icon={faTrashAlt} />;
+    const iconTrash = <FontAwesomeIcon icon={faTrashAlt}/>;
+    const iconCopy = <FontAwesomeIcon icon={faCopy}/>;
     return (
       <div className="color-field" style={{background: this.props.colors[this.props.number]}} onClick={ ()=>this.props.changeActiveColor(this.props.number) }>
         <div className="button-delete">
           <p className="button-delete-text" onClick={()=>this.props.deleteColor(this.props.number)}>{iconTrash}</p>
         </div>
+        <div className="button-copy">
+          <p className="button-copy-text">{iconCopy}</p>
+        </div>
         <span className="color-field-text">{ this.props.colors[this.props.number] }</span>
-        
-
       </div>
     );
   }
