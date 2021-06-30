76794c1069461554bbfef86916bca1b791f96f85
Aaron Kreuzer <a.kreuzer@oth-aw.de>
2021-06-30 17:20:33 +0200

    test changes
    Former-commit-id: c7db298b7f148a6de68c85e989d91d00b2c86485

--- sys-src/client/src/views/CreatePalette/createPalette.test.js
+++ sys-src/client/src/views/CreatePalette/createPalette.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CreatePalette from '../CreatePalette';
 
-it('renders without crashing', () => {
+it('renders without crashing - createPalette', () => {
   const div = document.createElement('div');
   ReactDOM.render(<CreatePalette />, div);
 });
