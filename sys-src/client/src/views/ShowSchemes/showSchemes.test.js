fa980232a5e14be7b6a64361f9c089b61c0c08d1
Aaron <a.kreuzer@oth-aw.de>
2021-06-30 17:20:33 +0200

    test changes

--- sys-src/client/src/views/ShowSchemes/showSchemes.test.js
+++ sys-src/client/src/views/ShowSchemes/showSchemes.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ShowSchemes from '../ShowSchemes';
 
-it('renders without crashing', () => {
+it('renders without crashing - showSchemes', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ShowSchemes />, div);
 });
