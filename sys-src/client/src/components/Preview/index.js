2082d1354dc16e9c23c3b68fe3570754ecb6bb67
Fabian Meiler <f.meiler1@oth-aw.de>
2021-06-30 20:02:02 +0200

    copy button added
    Former-commit-id: 3cd51369de1e3e3e4988eb18162e92102392f423

--- sys-src/client/src/components/Preview/index.js
+++ sys-src/client/src/components/Preview/index.js
@@ -80,7 +80,7 @@ const Logo = styled(ReactLogo)`
             <Container backgroundColor = {colors[2]}>
                <Logo color = {colors[0]}/>
                <Text color = {colors[0]}>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                </Text>
             </Container>
         </div>
