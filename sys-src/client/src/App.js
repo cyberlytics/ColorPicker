2fc9dfa8c189f18699c0d4d85992253e5ec2616c
Gassner, Anna-Lena <a.gassner@oth-aw.de>
2021-07-02 08:58:10 +0200

    deleted unused folders and components

--- sys-src/client/src/App.js
+++ sys-src/client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //Components
-import TabMenu from "./components/TabMenu"
+import TabMenu from "./parts/TabMenu"
 
 
 
--- sys-src/client/src/components/Rating/index.js
+++
@@ -1,4 +0,0 @@
-/**
- * Die Sterne zur Anzeige der bisherigen durchschnitts 
- * Bewertung bzw. Abgabe einer eigenen Bewertung.
- */
