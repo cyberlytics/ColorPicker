633d03151935a43c1a609e75e3134295e0be8715
Patrick Pohl <p.pohl@oth-aw.de>
2021-07-02 09:58:54 +0200

    bugfix: colorWheel
    Former-commit-id: 716ffc2d004631ae8d10308809c872480712ffb0

--- sys-src/client/src/components/ColorWheel/index.jsx
+++ sys-src/client/src/components/ColorWheel/index.jsx
@@ -12,17 +12,28 @@ import "./index.css";
   }
 
   colorPicker;
+  color;
 
   componentDidMount() {
     this.colorPicker = new iro.ColorPicker(this.el, {
       // Set the size of the color picker
       width: 350,
       // Set the initial color to pure red
-      color: this.props.colors,
+      color: this.props.colors[this.props.activeColor],
     });
-    this.colorPicker.on("color:change", this.props.changeColor);
+    this.colorPicker.on("color:change", ()=>this.props.updateHexValue(this.colorPicker.color.hexString));
+    this.colorPicker.on("color:setActive", function(color){
+      color = this.props.color[this.props.activeColor];
+    });
   };
 
+  componentDidUpdate(prevProps) {
+    
+    if(prevProps.colors[this.props.activeColor] !== this.props.colors[this.props.activeColor]){
+      
+      this.colorPicker.color.hexString = this.props.colors[this.props.activeColor];
+    }
+  }
 
   render() {
     return <div>
