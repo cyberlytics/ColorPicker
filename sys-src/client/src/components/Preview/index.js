77281eab556367c71a64aab7ccb1cf610f6e4131
Caroline Bengart <c.bengart@oth-aw.de>
2021-07-02 12:11:37 +0200

    adjust style of preview
    Former-commit-id: f6687d6dad2b618ece62c65c248e67ba7a4f031e

--- sys-src/client/src/components/Preview/index.js
+++ sys-src/client/src/components/Preview/index.js
@@ -16,54 +16,54 @@ const Subtitle = styled.p`
    font-family: sans-serif;
    letter-spacing: 0.2em;
    word-spacing: 30px;
-   margin-top: 15%;
-   margin-bottom: -15%;
    text-align: center;
    color: ${props => props.color};
 `
 
 const Header = styled.div`
-   padding: 3em;
-   background-color: ${props => props.backgroundColor}
+   padding: 1em;
+   background-color: ${props => props.backgroundColor};
 `
 const Container = styled.div`
    padding: 3em;
-   background-color: ${props => props.backgroundColor}
+   background-color: ${props => props.backgroundColor};
+   height: 550px;
 `
 
 const Text = styled.p`
   text-align: left;
-  color: ${props => props.color}
+  color: ${props => props.color};
 `
 
 const Logo = styled(ReactLogo)`
-   color: ${props => props.color}
+   color: ${props => props.color};
 `
 
 
  
  export function Preview (props) {
 
+   // Die ausgewählten Farben des Users
    const {colors} = props;
       
-   //wenn zwei Farben ausgewählt wurden
+   //Die Preview passt ihr Farbschema an, nachdem der User 2 Farben gewählt hat. 
    if(colors.length === 2){
       return(
-         <div>         
-            <Header backgroundColor = {colors[0]}>
-               <Title color = {colors[1]}>Welcome to this page</Title>
-               <Subtitle color = {colors[1]}>home gallery contact</Subtitle>
-            </Header>
-            <Container backgroundColor = {colors[1]}>
-               <Logo color = {colors[0]}/>
-               <Text color = {colors[0]}>
-                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-               </Text>
-            </Container>
-        </div>
+            <div>      
+               <Header backgroundColor = {colors[0]}>
+                  <Title color = {colors[1]}>Welcome to this page</Title>
+                  <Subtitle color = {colors[1]}>home gallery contact</Subtitle>
+               </Header>
+               <Container backgroundColor = {colors[1]}>
+                  <Logo color = {colors[0]}/>
+                  <Text color = {colors[0]}>
+                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                  </Text>
+               </Container>
+            </div>
       )
    } 
-   //wenn drei Farben ausgewählt wurden
+   //Die Preview passt ihr Farbschema an, nachdem der User 3 Farben gewählt hat. 
    else if(colors.length === 3){
       return(
          <div>         
@@ -80,7 +80,7 @@ const Logo = styled(ReactLogo)`
         </div>
       )
    } 
-   //wenn vier Farben ausgewählt wurden
+   //Die Preview passt ihr Farbschema an, nachdem der User 4 Farben gewählt hat. 
    else if(colors.length === 4){
       return(
          <div>         
@@ -97,7 +97,7 @@ const Logo = styled(ReactLogo)`
          </div>
       )
    } 
-   //wenn fünf Farben ausgewählt wurden
+   //Die Preview passt ihr Farbschema an, nachdem der User 5 Farben gewählt hat. 
    else {
       return(
       <div>         
